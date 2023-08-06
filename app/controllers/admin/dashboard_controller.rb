81e71f837a2ca8fbce4e39582ef7e58b3f63fa3c
Saim Amir <saimxamir@gmail.com>
2023-08-05 20:12:54 -0400

    implement admin dashboard showing counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name:ENV['AUTH_ADMIN_USERNAME'], password: ENV['AUTH_ADMIN_PASSWORD']
   
   def show
+    @category_count = Category.count
+    @product_count = Product.count
   end
 end
