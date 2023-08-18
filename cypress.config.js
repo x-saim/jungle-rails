e79a342ec1cd15be66271dd69ef1a42bd6611418
Saim Amir <saimxamir@gmail.com>
2023-08-17 20:25:14 -0400

    add db config and additional test cases

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
+  baseUrl: 'http://localhost:8000',
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+
   e2e: {
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser = {}, launchOptions) => {
--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -9,4 +9,8 @@ describe('Home Page', () => {
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 });
